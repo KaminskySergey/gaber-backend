17454e7102696b7b57a454aaa8be1b7473759287
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-02-29 22:39:33 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    exposedHeaders: 'set-cookie',
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_2],
+    exposedHeaders: "set-cookie",
   });
   app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle("Gaber backend")
+    .setDescription("The Gaber API description")
+    .setVersion("1.0")
+    .addTag("gaber")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(4200);
 }
 bootstrap();
